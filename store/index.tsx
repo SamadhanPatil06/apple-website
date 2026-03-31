aac08798c26acc388d92ffbff3322a4cd575e12a
SamadhanPatil06 <patilsamadhan584@gmail.com>
2026-03-31 12:23:18 +0530

    changes index.tsx

--- store/index.tsx
+++ store/index.tsx
@@ -1,8 +1,16 @@
-
 import { create } from "zustand";
 
+interface MacbookStore {
+  color: string;
+  setColor: (color: string) => void;
+  scale: number;
+  setScale: (scale: number) => void;
+  texture: string;
+  setTexture: (texture: string) => void;
+  reset: () => void;
+}
 
-const useMacbookStore = create((set) => ({
+const useMacbookStore = create<MacbookStore>((set) => ({
   color: "#2e2c2e",
   setColor: (color) => set({ color }),
 
