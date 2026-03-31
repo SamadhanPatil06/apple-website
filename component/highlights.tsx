9d5080e59aa0b162aacbd5e9bed8cdfeb1d81620
SamadhanPatil06 <patilsamadhan584@gmail.com>
2026-03-31 11:58:09 +0530

    create highlight component

--- component/highlights.tsx
+++ component/highlights.tsx
@@ -1,11 +1,68 @@
-import React from 'react'
 
-const highlights = () => {
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+
+const highlight = () => {
+  const isMobile = useMediaQuery({ query: "max-width: 1024px" });
+
+  useGSAP(() => {
+    gsap.to([".left-column", ".right-column"], {
+      scrollTrigger: {
+        trigger: "#highlights",
+        start: isMobile ? "bottom bottom" : "top top",
+      },
+      y: 0,
+      opacity: 1,
+      stagger: 0.5,
+      duration: 1,
+      ease: "power1.inOut",
+    });
+  });
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <section id="highlights">
+      <h2>There's never been a better time to upgrade.</h2>
 
-export default highlights
+      <h3>Here's what you get with the new Macbook Pro</h3>
+
+      <div className="masonry">
+        <div className="left-column">
+          <div>
+            <img src="/images/laptop.png" alt="Laptop" />
+            <p>Fly through demanding tasks up to 9.8x faster.</p>
+          </div>
+          <div>
+            <img src="/images/sun.png" alt="Sun  " />
+            <p>
+              A stunning <br /> Liquid Retina XDR <br /> display.
+            </p>
+          </div>
+        </div>
+
+        <div className="right-column">
+          <div className="apple-gradient">
+            <img src="/images/ai.png" alt="Ai" />
+            <p>
+              Build for <br /> <span>Apple Intelligence.</span>
+            </p>
+          </div>
+          <div>
+            <img src="/images/battery.png" alt="Battery" />
+            <p>
+              Up to <span className="green-gradient"> 14 more hours </span>{" "}
+              battery life.{" "}
+              <span className="text-dark-100">
+                {" "}
+                (Up to 24 hours total.) {"   "}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default highlight;
