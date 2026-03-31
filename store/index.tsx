7835f4375afa09734c885e6f6b22db7b9225a9a6
SamadhanPatil06 <patilsamadhan584@gmail.com>
2026-03-31 10:48:06 +0530

    create features component

--- store/index.tsx
+++ store/index.tsx
@@ -1,19 +1,19 @@
-import { create } from 'zustand';
 
-interface MacbookStore {
-  color: string;
-  setColor: (color: string) => void;
-  scale: number;
-  setScale: (scale: number) => void;
-  reset: () => void;
-}
+import { create } from "zustand";
 
-const useMacbookStore = create<MacbookStore>((set) => ({
-  color: '#2e2c2e',
+
+const useMacbookStore = create((set) => ({
+  color: "#2e2c2e",
   setColor: (color) => set({ color }),
+
   scale: 0.08,
   setScale: (scale) => set({ scale }),
-  reset: () => set({ color: '#2e2c2e', scale: 0.08 }),
-}))
+
+  texture: "/videos/feature-1.mp4",
+  setTexture: (texture) => set({ texture }),
+
+  reset: () =>
+    set({ color: "#2e2c2e", scale: 0.08, texture: "/videos/feature-1.mp4" }),
+}));
 
 export default useMacbookStore;
